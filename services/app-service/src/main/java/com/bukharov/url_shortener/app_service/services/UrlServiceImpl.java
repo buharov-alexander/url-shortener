3a624dbeca66750256cd75ddafa8cc1ad44f6ebc
Alexander Bukharov <buharovsasha@yandex.ru>
2026-01-27 23:30:18 +0700

    Save UrlEntity

--- services/app-service/src/main/java/com/bukharov/url_shortener/app_service/services/UrlServiceImpl.java
+++ services/app-service/src/main/java/com/bukharov/url_shortener/app_service/services/UrlServiceImpl.java
@@ -1,5 +1,7 @@
 package com.bukharov.url_shortener.app_service.services;
 
+import com.bukharov.url_shortener.app_service.repository.UrlEntity;
+import com.bukharov.url_shortener.app_service.repository.UrlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,10 +10,17 @@ class UrlServiceImpl implements UrlService {
 
 	@Autowired
 	private IdGeneratorService idGeneratorService;
+	@Autowired
+	private UrlRepository urlRepository;
 
 	@Override
 	public String saveUniqueCode(String originalUrl) {
-		return idGeneratorService.generateUniqueCode();
+		String uniqueCode = idGeneratorService.generateUniqueCode();
+		UrlEntity entity = new UrlEntity();
+		entity.setOriginalUrl(originalUrl);
+		entity.setUniqueCode(uniqueCode);
+		urlRepository.save(entity);
+		return uniqueCode;
 	}
 
 	@Override
