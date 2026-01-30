2a5f021d29d8df266c043578fe3cd8597cb65c56
Alexander Bukharov <buharovsasha@yandex.ru>
2026-01-30 22:49:49 +0700

    fix spring test

--- services/app-service/src/test/java/com/bukharov/url_shortener/app_service/AppServiceApplicationTests.java
+++ services/app-service/src/test/java/com/bukharov/url_shortener/app_service/AppServiceApplicationTests.java
@@ -1,10 +1,20 @@
 package com.bukharov.url_shortener.app_service;
 
+import com.bukharov.url_shortener.app_service.repository.UrlRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.cassandra.autoconfigure.CassandraAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+		CassandraAutoConfiguration.class,
+})
 class AppServiceApplicationTests {
+
+	@MockitoBean
+	private UrlRepository urlRepository;
 
 	@Test
 	void contextLoads() {
