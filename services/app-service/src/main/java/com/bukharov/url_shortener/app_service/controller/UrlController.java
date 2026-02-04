ec42a902f5d7cebe301b3c12ff47eaaba68c5b62
Alexander Bukharov <buharovsasha@yandex.ru>
2026-02-04 22:44:49 +0700

    Refactor shortenUrl method in UrlController to include HttpServletRequest for generating full shortened URL

--- services/app-service/src/main/java/com/bukharov/url_shortener/app_service/controller/UrlController.java
+++ services/app-service/src/main/java/com/bukharov/url_shortener/app_service/controller/UrlController.java
@@ -1,5 +1,6 @@
 package com.bukharov.url_shortener.app_service.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,13 +13,15 @@ import com.bukharov.url_shortener.app_service.services.UrlService;
 @RestController
 @RequestMapping("/url")
 public class UrlController {
-	
+
 	@Autowired
 	private UrlService urlService;
 
-	@PostMapping("/shorten")
-	public UrlDTO shortenUrl(@RequestBody UrlDTO urlDTO) {
-		return new UrlDTO(urlService.saveUniqueCode(urlDTO.url()));
+	@PostMapping
+	public UrlDTO shortenUrl(@RequestBody UrlDTO urlDTO, HttpServletRequest request) {
+		String uniqueCode = urlService.saveUniqueCode(urlDTO.url());
+		String shortenUrl = String.format("%s/%s", request.getRequestURL(), uniqueCode);
+		return new UrlDTO(shortenUrl);
 	}
 
 	@GetMapping("/{uniqueCode}")
