36ceefe719f7c42a0441eaa8e92ede1b07476ce6
Alexander Bukharov <buharovsasha@yandex.ru>
2026-02-07 15:01:28 +0700

    Refactor redirectToOriginalUrl method in UrlController to return ResponseEntity with HTTP status and location

--- services/app-service/src/main/java/com/bukharov/url_shortener/app_service/controller/UrlController.java
+++ services/app-service/src/main/java/com/bukharov/url_shortener/app_service/controller/UrlController.java
@@ -1,6 +1,10 @@
 package com.bukharov.url_shortener.app_service.controller;
 
+import java.net.URI;
+
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +29,10 @@ public class UrlController {
 	}
 
 	@GetMapping("/{uniqueCode}")
-	public UrlDTO redirectToOriginalUrl(@PathVariable String uniqueCode) {
-		return new UrlDTO(urlService.getByUniqueCode(uniqueCode));
+	public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String uniqueCode) {
+		return ResponseEntity
+				.status(HttpStatus.MOVED_PERMANENTLY)
+				.location(URI.create(urlService.getByUniqueCode(uniqueCode)))
+				.build();
 	}
 }
